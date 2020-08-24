e212f963027264cf292b140a5dca86d0094abdd4
Muhammad Fauzan Fikri <mfauzanfff@gmail.com>
2020-08-24 11:37:15 +0700

    initial push

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/timestamp/:dateString?", (req, res) => {
+  let date;
+//   console.log("isi param datestring adalah " + req.params.dateString);
+
+  const checkInt = (reqDate) => {
+    let data = parseInt(reqDate, 10);
+    if (reqDate == data) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const getDate = (reqDate) => {
+    if (reqDate) {
+      if (checkInt(reqDate) === true) {
+        date = new Date(parseInt(reqDate, 10));
+        // console.log("isi variabel " + date);
+      } else {
+        date = new Date(reqDate);
+      }
+    } else {
+      date = new Date();
+    }
+    // console.log(date);
+    return date;
+  };
+
+  if (getDate(req.params.dateString) == "Invalid Date") {
+    res.json({
+      error: "Invalid Date",
+    });
+  } else {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    });
+  }
+});
+
+app.listen(3000, () => console.log("listening on 3000"));
